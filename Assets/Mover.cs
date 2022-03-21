d45036774b8cb04ba0294099783bd9e7475d86e9
Muhammad <muhammadhullah23@gmail.com>
2022-03-20 20:06:31 -0400

    practice building methods

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// void Start() {} = return nothing
 public class Mover : MonoBehaviour
 {
-    float moveSpeed = 65f;
+    [SerializeField] float moveSpeed = 65f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,12 @@ public class Mover : MonoBehaviour
         // Using Time.deltaTime Unity can tell us how long each framework took to execute.
         // When we multiply something by Time.deltaTime it makes our game "frame rate independent"
         transform.Translate(xValue, 0, zValue);
+    }
+
+    void PrintInstruction() 
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Use arrow keys to move around");
+        Debug.Log("Avoid obstacles or get sent back to the start");
     }
 }
