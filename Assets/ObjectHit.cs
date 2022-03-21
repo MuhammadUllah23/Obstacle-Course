bcc380d1fa8ee2c5b4f46e8089c8f8c7d4b0b402
Muhammad <muhammadhullah23@gmail.com>
2022-03-21 02:33:19 -0400

    change color of wall when being hit for first time

--- Assets/ObjectHit.cs
+++ Assets/ObjectHit.cs
@@ -7,5 +7,6 @@ public class ObjectHit : MonoBehaviour
    private void OnCollisionEnter(Collision other) 
    {
        Debug.Log("Bumped"); 
+       GetComponent<MeshRenderer>().material.color = Color.green;
    }
 }
