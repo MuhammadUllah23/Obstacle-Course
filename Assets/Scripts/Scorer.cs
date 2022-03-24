b0c6b3bd9543d30c9503e582b30a77ac1637cec2
Muhammad <muhammadhullah23@gmail.com>
2022-03-24 16:33:01 -0400

    player only resets when colliding with objects with Enemy tags

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -7,15 +7,15 @@ public class Scorer : MonoBehaviour
     int hits = 0;
 
     float xStart = 0f;
-    float yStart = 2.234f;
+    float yStart = 1.1f;
     float zStart = -26.124f;
     private void OnCollisionEnter(Collision other) 
     {
-        if(other.gameObject.tag == "Ground") {
-            
+        if(other.gameObject.tag == "Enemy") {
+            hits++;
+            Debug.Log("Bumped this many times" + hits); 
+            transform.position = new Vector3(xStart, yStart, zStart); 
         }
-        hits++;
-       Debug.Log("Bumped this many times" + hits); 
-       transform.position = new Vector3(xStart, yStart, zStart);
+        
     }
 }
