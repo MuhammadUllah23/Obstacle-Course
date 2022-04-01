edca51ae9bc8ecbfe7b8cd284a7371a7c2d42db7
Muhammad <muhammadhullah23@gmail.com>
2022-04-01 02:14:00 -0400

    winning x, y, and z variables

---
+++ Assets/Scripts/Winner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    float xStart = 0f;
+    float yStart = 200f;
+    float zStart = 0f;
+}
