981c9a69a59c4f466374789de317ae933d25abbf
Muhammad <muhammadhullah23@gmail.com>
2022-04-02 17:10:29 -0400

    all original positions of enemy gameObjects are stored in jagged arrays

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -13,15 +13,39 @@ public class Scorer : MonoBehaviour
     float xFinish;
     float yFinish;
     float zFinish;
+    GameObject[] enemies;
+    float[][] originalPositions = new float[][];
+
+    void Start() {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        int i = 0;
+        foreach (GameObject enemy in enemies){
+            float[] positions = new float[3];
+            
+            positions[0] = enemy.transform.position.x;
+            positions[1] = enemy.transform.position.y;
+            positions[2] = enemy.transform.position.z;
+
+            originalPositions[i] = positions;
+            
+        }
+    }
     private void OnCollisionEnter(Collision other) {
         xFinish = GameObject.Find("Winner Platform").transform.position.x;
         yFinish = GameObject.Find("Winner Platform").transform.position.y + 10;
         zFinish = GameObject.Find("Winner Platform").transform.position.z;
 
+        
+        
+
         if(other.gameObject.tag == "Enemy") {
             hits++;
             Debug.Log("Bumped this many times: " + hits); 
             transform.position = new Vector3(xStart, yStart, zStart); 
+            foreach (GameObject enemy in enemies){
+
+            }
+            
         } else if(other.gameObject.tag == "Winner") {
             Debug.Log("YOU WON AFTER " + hits + " TRIES!"); 
             transform.position = new Vector3(xFinish, yFinish, zFinish); 
