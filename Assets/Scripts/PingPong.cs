a88e2e8da1691a902217269d5448a1e805bcf423
Muhammad <muhammadhullah23@gmail.com>
2022-04-05 02:43:06 -0400

    Pinger obstacle moves in z axis

--- Assets/Scripts/PingPong.cs
+++ Assets/Scripts/PingPong.cs
@@ -12,14 +12,14 @@ public class PingPong : MonoBehaviour
         min=transform.position.x;
         max=transform.position.x+4;
         
-   
+//    
     }
    
     // Update is called once per frame
     void Update () {
         
         if (gameObject.name.StartsWith("Pinger")) {
-            transform.position = new Vector3(Mathf.PingPong(Time.time*10,max-min)+min, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time*10,max-min)+min);
         } else if(gameObject.name.StartsWith("Ping Pong Cube")) {
             transform.position = new Vector3(Mathf.PingPong(Time.time*5,max-min)+min, transform.position.y, transform.position.z);
         }
